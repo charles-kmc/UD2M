afa8512ce3fb7cc77a9328371d1e5ceff857dd46
Charlesquin <cmk2000@hw.ac.uk>
2024-08-10 22:30:30 +0100

    lora_parametrisation.py updated

--- utils_lora/lora_parametrisation.py
+++ utils_lora/lora_parametrisation.py
@@ -1,20 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.utils.parametrize as P
-from guided_diffusion.unet import *
 import copy
 
 
 # The following code is used to parametrise the layer
 class LoRaParametrisation(nn.Module):
-    def __init__(
-                self, 
-                in_channel, 
-                out_channel, 
-                rank = 2, 
-                alpha = 1, 
-                device = "cpu"
-            ):
+    def __init__(self, in_channel, out_channel, rank = 2, alpha = 1, device = "cpu"):
         super(LoRaParametrisation, self).__init__()
         self.in_channel = in_channel
         self.out_channel = out_channel
@@ -23,10 +15,8 @@ class LoRaParametrisation(nn.Module):
         self.alpha = alpha
         self.lora_A = nn.Parameter(torch.zeros(self.in_channel, self.rank, device = self.device))
         self.lora_B = nn.Parameter(torch.zeros(self.rank, self.out_channel, device = self.device))
-        
         nn.init.normal_(self.lora_A, mean = 0, std = 1)
         nn.init.zeros_(self.lora_B)
-        
         self.scale = self.alpha / self.rank
         self.enabled = True
         
@@ -35,85 +25,25 @@ class LoRaParametrisation(nn.Module):
             return original_weights + torch.matmul(self.lora_A, self.lora_B).view(original_weights.shape) * self.scale
         return original_weights
 
-# The following code is used to parametrise the layer   
-def layer_parametrisation(
-                        layer, 
-                        rank = 2, 
-                        device = "cpu"
-                    ):
-    
+def layer_parametrisation(layer, rank = 2, device = "cpu"):
     in_channel, out_channel = layer.weight.squeeze().shape
-    return LoRaParametrisation(
-                        in_channel, 
-                        out_channel, 
-                        rank = rank, 
-                        device = device
-                    )
+    return LoRaParametrisation(in_channel, out_channel, rank = rank, device = device)
 
-# apply LoRa parametrisation
-def apply_lora_parametrization(
-                        block, 
-                        rank, 
-                        device
-                    ):
-    for block_att in block.children():
-        if isinstance(block_att, AttentionBlock):
-            # qkv layer followed by lora layer
-            conv1d_qkv = block_att.qkv
-            P.register_parametrization(
-                                conv1d_qkv, 
-                                "weight", 
-                                layer_parametrisation(
-                                    conv1d_qkv, 
-                                    rank=rank, 
-                                    device=device
-                                )
-                            )
-            # projection layer followed by lora layer
-            conv1d_proj_out = block_att.proj_out
-            P.register_parametrization(
-                                conv1d_proj_out, 
-                                "weight", 
-                                layer_parametrisation(
-                                    conv1d_proj_out, 
-                                    rank=rank, 
-                                    device=device
-                                )
-                            )
+def apply_lora_parametrization(model, rank, device):
+    #   loop through all the layers of the model
+    for name, layer in model.named_modules():
+    #    Check if the layer name matches "to_k", "to_q", or "to_v"
+        if name.endswith("qkv") or name.endswith("proj_out"):
+            P.register_parametrization(layer, "weight", layer_parametrisation(layer, rank=rank,  device=device))           
 
-# The following code is used to apply LoRa to the model
-def LoRa_model(
-            model, 
-            input_block_layer_lora = [10, 14], 
-            middle_block_layer_lora = [1], 
-            output_block_layer_lora = [3, 6], 
-            rank = 2, 
-            device = "cpu"
-        ):
-        
+def LoRa_model(model, rank = 2, device = "cpu"):
     # Apply LoRA parametrization to input blocks
-    for ii, block in enumerate(model.input_blocks):
-        if ii in input_block_layer_lora:
-            apply_lora_parametrization(block, rank, device)
-    
-    # Apply LoRA parametrization to the middle block
-    assert len(middle_block_layer_lora) == 1, "Middle block has only 1 block with one self attention block."
-    if middle_block_layer_lora is not None:
-        apply_lora_parametrization(model.middle_block, rank, device)
-
-    # Apply LoRA parametrization to output blocks
-    for ii, block in enumerate(model.output_blocks):
-        if ii in output_block_layer_lora:
-            apply_lora_parametrization(block, rank, device)       
-    
-    # freeze non-LoRA parameters
+    apply_lora_parametrization(model, rank, device)
     print(f'Number of Layers frozen: {num_frozen_layers(model)}')
-    
     total_parameters_non_lora, total_parameters_lora = total_parameters(model)
     print(f"Total trainable parameters of the model: {total_parameters_non_lora} (non-LoRa) vs {total_parameters_lora} (LoRa) Ratio: {(total_parameters_lora/total_parameters_non_lora)*100:.2f}% of the original model")
     return model
 
-# The following code is used to count the number of parameters in the model  
 def total_parameters(model):
     n_frozen = 0
     n_lora = 0
